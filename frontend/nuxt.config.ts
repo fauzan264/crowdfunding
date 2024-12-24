3346404f9f4302760c845cd5d518619cdbd2b13b
Fauzan <fauzan.c4@gmail.com>
2024-12-25 01:05:07 +0700

    configuration tailwindcss

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  css: [
+    '@/assets/css/tailwind.css', // Add your Tailwind CSS here
+  ],
+  app: {
+    head: {
+      title: 'Build With Angga - Crowdfunding App',
+      meta: [
+        { charset: 'UTF-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'This is a description of my Nuxt app' },
+        { name: 'author', content: 'fauzan264' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { 
+          rel: 'stylesheet', 
+          href: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap' 
+        },
+      ],
+      script: [],
+    },
+  },
 })
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // content: [
+  //   "./components/**/*.{js,vue,ts}",
+  //   "./layouts/**/*.vue",
+  //   "./pages/**/*.vue",
+  //   "./plugins/**/*.{js,ts}",
+  //   "./app.vue",
+  //   "./error.vue",
+  // ],
+  content: [
+    './pages/**/*.{vue,js,ts}',
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.{vue,js,ts}',
+    './plugins/**/*.{js,ts}', // If you have any plugins
+    './nuxt.config.{js,ts}' // Include Nuxt config if using Tailwind in it
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        outline: "0 0 0 1px rgba(66, 153, 225, .5)",
+      },
+      color: {
+        "orange-button": "#FF872E",
+        "green-button": "#1ABC9C",
+        "purple-hover": "#4C52F8",
+        "purple-hover-stroke": "#8286FF",
+        "purple-progress": "#3B41E3",
+      },
+      borderRadius: {
+        20: "20px",
+      }
+    },
+  },
+  fontFamily: {
+    sans: ['Poppins', 'sans-serif'],
+  },
+  plugins: [],
+}
+
