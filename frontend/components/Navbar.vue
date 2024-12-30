b396e659195b6325f6ca1a885e693d8fdb094f47
Fauzan <fauzan.c4@gmail.com>
2024-12-31 02:50:29 +0700

    update navbar

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -34,7 +34,7 @@
                 <nuxt-link
                     to="/register"
                     class="inline-block bg-transparent border-white border hover:bg-white hover:bg-opacity-25 text-white font-light w-40 text-center px-6 py-1 text-lg rounded-full mr-4"
-                ></nuxt-link>
+                >Sign Up</nuxt-link>
             </li>
             <li>
                 <nuxt-link
