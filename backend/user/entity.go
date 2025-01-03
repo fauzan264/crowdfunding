2d014f51abf757947d1f89ce7d6bd32495f8e54b
Fauzan <fauzan.c4@gmail.com>
2025-01-04 05:42:06 +0700

    update indentation

--- backend/user/entity.go
+++ backend/user/entity.go
@@ -7,15 +7,15 @@ import (
 )
 
 type User struct {
-	ID uuid.UUID
-	Name string
-	Occupation string
-	Email string
-	Password string
-	AvatarFileName string
-	Role string
-	CreatedBy uuid.UUID
-	CreatedAt time.Time
-	UpdatedBy uuid.UUID
-	UpdatedAt time.Time
+	ID 				uuid.UUID
+	Name 			string
+	Occupation 		string
+	Email 			string
+	Password 		string
+	AvatarFileName 	string
+	Role 			string
+	CreatedBy 		uuid.UUID
+	CreatedAt 		time.Time
+	UpdatedBy 		uuid.UUID
+	UpdatedAt 		time.Time
 }
