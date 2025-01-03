d228bda1495c58fa4bf2b0e07c4f3722ab241398
Fauzan <fauzan.c4@gmail.com>
2025-01-04 05:42:55 +0700

    create entity campaign

--- backend/campaign/entity.go
+++ backend/campaign/entity.go
@@ -1,1 +1,24 @@
 package campaign
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Campaign struct {
+	ID 					uuid.UUID
+	UserID				uuid.UUID
+	Title				string
+	ShortDescription 	string
+	Description			string
+	GoalAmount			int
+	CurrentAmount		int
+	Perks				string
+	BeckerCount			int
+	Slug				string
+	CreatedBy			uuid.UUID
+	CreatedAt			time.Time
+	UpdatedBy			uuid.UUID
+	UpdatedAt			uuid.UUID
+}
