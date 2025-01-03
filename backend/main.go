525d2b8b1800adaf039b41f5595c396159bec7b4
Fauzan <fauzan.c4@gmail.com>
2025-01-04 00:16:51 +0700

    update main.go

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/fauzan264/crowdfunding/backend/auth"
+	"github.com/fauzan264/crowdfunding/backend/config"
 	"github.com/fauzan264/crowdfunding/backend/handler"
-	"github.com/fauzan264/crowdfunding/backend/helper"
 	"github.com/fauzan264/crowdfunding/backend/user"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
 
 func main() {
@@ -21,18 +18,7 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	var(
-		DbHost = helper.GetEnv("DB_HOST", "db")
-		DBPort = helper. GetEnv("DB_PORT", "3306")
-		DbUser = helper.GetEnv("DB_USER", "root")
-		DbPassword = helper.GetEnv("DB_PASSWORD", "root")
-	)
-
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local", DbUser, DbPassword, DbHost, DBPort)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+	db := config.InitDatabase()
 
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
