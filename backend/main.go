ee5acf853006edf47249c3776f7af375e6795f92
Fauzan <fauzan.c4@gmail.com>
2025-01-12 17:42:19 +0700

    clean main.go file

--- backend/main.go
+++ backend/main.go
@@ -1,100 +1,45 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"strings"
-
 	"github.com/fauzan264/crowdfunding/backend/auth"
 	"github.com/fauzan264/crowdfunding/backend/campaign"
 	"github.com/fauzan264/crowdfunding/backend/config"
 	"github.com/fauzan264/crowdfunding/backend/handler"
-	"github.com/fauzan264/crowdfunding/backend/helper"
+	"github.com/fauzan264/crowdfunding/backend/middleware"
 	"github.com/fauzan264/crowdfunding/backend/user"
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt/v5"
-	"github.com/google/uuid"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	// Load file .env
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	db := config.InitDatabase()
 
+	// Repositories
 	userRepository := user.NewRepository(db)
 	campaignRepository := campaign.NewRepository(db)
 	
+	// Services
 	userService := user.NewService(userRepository)
 	campaignService := campaign.NewService(campaignRepository)
 	authService := auth.NewService()
 
+	// Handlers
 	userHandler := handler.NewUserHandler(userService, authService)
 	campaignHandler := handler.NewCampaignHandler(campaignService)
 
+	// Initialize router
 	router := gin.Default()
 	router.Static("/images", "./images")
 	
 	api := router.Group("/api/v1")
 
+	// User-related routes
 	api.POST("/users", userHandler.RegisterUser)
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checkers", userHandler.CheckEmailAvailibility)
-	api.POST("/avatars", authMiddleware(authService, userService), userHandler.UploadAvatar)
+	api.POST("/avatars", middleware.AuthMiddleware(authService, userService), userHandler.UploadAvatar)
 
-	api.GET("/campaigns/", campaignHandler.GetHandler)
+	// Campaign-related routes
+	api.GET("/campaigns", campaignHandler.GetHandler)
 
 	router.Run()
-}
-
-func authMiddleware(authService auth.Service, userService user.Service) gin.HandlerFunc {
-	return func (c *gin.Context) {
-		authHeader := c.GetHeader("Authorization")
-	
-		if !strings.Contains(authHeader, "Bearer") {
-			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
-			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
-			return
-		}
-	
-		tokenString := ""
-		arrayToken := strings.Split(authHeader, " ")
-		if len(arrayToken) == 2 {
-			tokenString = arrayToken[1]
-		}
-	
-		token, err := authService.ValidateToken(tokenString)
-		if err != nil {
-			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
-			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
-			return
-		}
-
-		claim, ok := token.Claims.(jwt.MapClaims)
-
-		if !ok || !token.Valid {
-			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
-			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
-			return
-		}
-
-		userID, err := uuid.Parse(claim["user_id"].(string))
-		if err != nil {
-			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
-			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
-			return
-		}
-
-		user, err := userService.GetUserByID(userID)
-		if err != nil {
-			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
-			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
-			return
-		}
-		c.Set("currentUser", user)
-	}
 }
