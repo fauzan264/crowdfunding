d43256b6676be408c54cde5fe8626274089f8441
Fauzan <fauzan.c4@gmail.com>
2025-01-27 22:39:34 +0700

    create entity transaction

--- backend/transaction/entity.go
+++ backend/transaction/entity.go
@@ -1,1 +1,20 @@
 package transaction
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Transaction struct {
+	ID 				uuid.UUID
+	CampaignID 		uuid.UUID
+	UserID			uuid.UUID
+	Amount			int
+	Status			string
+	Code			string
+	CreatedBy		uuid.UUID
+	CreatedAt		time.Time
+	UpdatedBy		uuid.UUID
+	UpdatedAt		time.Time
+}
